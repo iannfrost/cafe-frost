ca73db4bef1f3c15330338ab40750c3c79806280
Ian <ian.niles.frost@gmail.com>
2025-05-23 20:31:27 -0400

    Initial commit with skeleton of site
    index, menu, contact, about pages included with image assets

---
+++ scripts.js
@@ -0,0 +1,27 @@
+document.getElementById("menu-btn").addEventListener("click", () => {
+  const menu = document.getElementById("mobile-menu");
+  menu.classList.toggle("hidden");
+});
+
+
+var swiper = new Swiper('.swiper-container', {
+  slidesPerView: 1,
+  spaceBetween: 20,
+  loop: true,
+  pagination: {
+    el: '.swiper-pagination',
+    clickable: true,
+  },
+  navigation: {
+    nextEl: '.swiper-button-next',
+    prevEl: '.swiper-button-prev',
+  },
+  breakpoints: {
+    768: {
+      slidesPerView: 2,
+    },
+    1024: {
+      slidesPerView: 3,
+    }
+  }
+});
